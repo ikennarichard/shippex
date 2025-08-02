c4bbe0561ec1d429ee86be580c1a70cb93a8b99e
Ikenna Richard <oguejioforrichard@gmail.com>
2025-08-02 18:15:27 +0100

    refactor: root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,27 +1,34 @@
+import WelcomeScreen from "@/components/WelcomeScreen";
 import { AuthProvider } from "@/context/AuthContext";
-import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "react-native-reanimated";
-import '../global.css'
+import "../global.css";
 
-export {
-  ErrorBoundary,
-} from "expo-router";
+export { ErrorBoundary } from "expo-router";
 
 export const unstable_settings = {
   initialRouteName: "(tabs)",
 };
 
+SplashScreen.setOptions({
+  fade: true,
+  duration: 600,
+});
+
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-    ...FontAwesome.font,
+    SFPro_Regular: require("../assets/fonts/SF-Pro.ttf"),
+    SFPro_Bold: require("../assets/fonts/SF-Pro-Text-Bold.otf"),
+    SFPro_Semibold: require("../assets/fonts/SF-Pro-Text-Semibold.otf"),
+    SFPro_Light: require("../assets/fonts/SF-Pro-Text-Light.otf"),
   });
+
+  const [animationDone, setAnimationDone] = useState(false);
 
   useEffect(() => {
     if (error) throw error;
@@ -29,12 +36,16 @@ export default function RootLayout() {
 
   useEffect(() => {
     if (loaded) {
-      SplashScreen.hideAsync();
+      setTimeout(async () => {
+        await SplashScreen.hideAsync();
+      }, 1000);
     }
   }, [loaded]);
 
-  if (!loaded) {
-    return null;
+  if (!loaded || !animationDone) {
+    return loaded ? (
+      <WelcomeScreen onFinish={() => setAnimationDone(true)} />
+    ) : null;
   }
 
   return (
